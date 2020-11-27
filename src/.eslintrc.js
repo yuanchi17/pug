54b649e9663711a935bb7e39e242450903823828
李元琦 <yuan.chi@program.com.tw>
2020-11-27 11:00:11 +0800

    eslint

---
+++ src/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  plugins: [
+    'pug',
+  ],
+  env: {
+    es6: true,
+    browser: true,
+  },
+  extends: [
+    'standard',
+  ],
+  globals: {
+    _: 'readonly',
+    axios: 'readonly',
+    Papa: 'readonly',
+    Swal: 'readonly',
+    Vue: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'eol-last': ['error', 'never'],
+    'no-return-await': 0, // 0 = off, 1 = warn, 2 = error
+    'spaced-comment': ['error', 'always', { markers: ['-'] }],
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'only-multiline',
+    }],
+  },
+}
